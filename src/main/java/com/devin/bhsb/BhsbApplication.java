14dc58136bae042f27c362b0aac85370f6e83e0e
Devin Herath <devdhera@gmail.com>
2018-10-21 19:04:02 +0530

    Initial commit

---
+++ src/main/java/com/devin/bhsb/BhsbApplication.java
@@ -0,0 +1,26 @@
+package com.devin.bhsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.convert.Jsr310Converters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+@EntityScan(basePackageClasses = {
+		BhsbApplication.class,
+		Jsr310Converters.class
+})
+public class BhsbApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhsbApplication.class, args);
+	}
+}
