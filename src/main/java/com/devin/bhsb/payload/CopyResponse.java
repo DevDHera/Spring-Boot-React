e914bff1395d0833d6b4bbc769301fdf36faf3a0
Devin Herath <devdhera@gmail.com>
2018-10-30 13:31:23 +0530

    Added CopyResponse Payload

---
+++ src/main/java/com/devin/bhsb/payload/CopyResponse.java
@@ -0,0 +1,31 @@
+package com.devin.bhsb.payload;
+
+public class CopyResponse {
+    private long id;
+    private Boolean status;
+    private long borrowCount;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public long getBorrowCount() {
+        return borrowCount;
+    }
+
+    public void setBorrowCount(long borrowCount) {
+        this.borrowCount = borrowCount;
+    }
+}
