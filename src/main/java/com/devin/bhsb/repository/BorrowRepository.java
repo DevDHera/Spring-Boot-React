55bc09bad31e9fb9d13954f9f8fa87f859637c45
Devin Herath <devdhera@gmail.com>
2018-11-01 00:18:24 +0530

    Added custom queries for count

--- src/main/java/com/devin/bhsb/repository/BorrowRepository.java
+++ src/main/java/com/devin/bhsb/repository/BorrowRepository.java
@@ -1,7 +1,9 @@
 package com.devin.bhsb.repository;
 
+import com.devin.bhsb.model.AuthorBorrowCount;
 import com.devin.bhsb.model.Book;
 import com.devin.bhsb.model.Borrow;
+import com.devin.bhsb.model.CopyBorrowCount;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,6 +15,18 @@ import java.util.List;
 
 @Repository
 public interface BorrowRepository extends JpaRepository<Book, Long> {
+    @Query("SELECT NEW com.example.bhsb.model.CopyBorrowCount(b.copy.id, count(b.id)) FROM Book b WHERE b.book.id in :bookIds GROUP BY b.copy.id")
+    List<CopyBorrowCount> countByBookIdInGroupByCopyId(@Param("pollIds") List<Long> pollIds);
+
+    @Query("SELECT NEW com.example.bhsb.model.CopyBorrowCount(b.copy.id, count(b.id)) FROM Book b WHERE b.book.id = :bookId GROUP BY b.copy.id")
+    List<CopyBorrowCount> countByBookIdGroupByCopyId(@Param("pollId") Long pollId);
+
+    @Query("SELECT NEW com.example.bhsb.model.AuthorBorrowCount(b.author.id, count(b.id)) FROM Book b WHERE b.book.id in :bookIds GROUP BY b.author.id")
+    List<AuthorBorrowCount> countByBookIdInGroupByAuthorId(@Param("pollIds") List<Long> pollIds);
+
+    @Query("SELECT NEW com.example.bhsb.model.AuthorBorrowCount(b.author.id, count(b.id)) FROM Book b WHERE b.book.id = :bookId GROUP BY b.author.id")
+    List<AuthorBorrowCount> countByBookIdGroupByAuthorId(@Param("pollId") Long pollId);
+
     @Query("SELECT b FROM Borrow b WHERE b.user.id = :userId AND b.book.id IN :bookIds")
     List<Borrow> findByUserIdAndBookIdIn(@Param("userId") Long userId, @Param("bookIds") List<Long> bookIds);
 
